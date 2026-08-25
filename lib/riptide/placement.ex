0068bb387c4756c6479e158338674cee6d5eaa0c
Julian Nalenz <julian.nalenz@divizend.com>
2026-08-25 09:50:53 +0000

    Placement.propose_nodes/2: always include the declaring node (RabbitMQ precedent)

--- lib/riptide/placement.ex
+++ lib/riptide/placement.ex
@@ -19,9 +19,13 @@ defmodule Riptide.Placement do
 
   @replication_factor 3
 
-  @spec propose_nodes(pos_integer()) :: [node()]
-  def propose_nodes(replication_factor \\ @replication_factor) do
-    select_nodes(Node.list() ++ [node()], replication_factor)
+  @spec propose_nodes(pos_integer(), [node()]) :: [node()]
+  def propose_nodes(replication_factor \\ @replication_factor, peers \\ Node.list()) do
+    local = node()
+    remaining = max(replication_factor - 1, 0)
+    other_candidates = peers -- [local]
+
+    [local | select_nodes(other_candidates, remaining)]
   end
 
   @spec select_nodes([node()], pos_integer()) :: [node()]
